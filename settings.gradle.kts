64c82b23301feabd8fa7a0906675a8b8239f6b86
alipbekovich <erbolruskulbekvipp@gmail.com>
2023-12-18 01:01:10 +0600

    commit eken

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,12 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
 
 rootProject.name = "CatchOk_Apps"
 include(":app")
