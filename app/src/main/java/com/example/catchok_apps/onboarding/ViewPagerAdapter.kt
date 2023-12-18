ce93e5673b5a942d80fbcffedc19662eb6596532
IslamBratan <116435912+IslamBratan@users.noreply.github.com>
2023-12-18 17:06:53 +0600

    onboarding ends

---
+++ app/src/main/java/com/example/catchok_apps/onboarding/ViewPagerAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.catchok_apps.onboarding
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewbinding.ViewBinding
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import package com.example.catchok_apps.onboarding.base.BaseFragment
+
+class ViewPagerAdapter(
+    list: ArrayList<BaseFragment<out ViewBinding>>,
+    fm:FragmentManager, lifecycle:Lifecycle
+): FragmentStateAdapter(fm, lifecycle) {
+
+    private val fragmentList:ArrayList<BaseFragment<out ViewBinding>> = list
+    override fun getItemCount(): Int {
+        return fragmentList.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragmentList[position]
+    }
+}
