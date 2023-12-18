d0f4580073e560543cc0beaa13db19e2c3c7a8df
IslamBratan <116435912+IslamBratan@users.noreply.github.com>
2023-12-18 18:03:55 +0600

    utilities add

---
+++ app/src/main/java/com/example/catchok_apps/utilities/dailyCalories.kt
@@ -0,0 +1,33 @@
+package com.example.catchok_apps.utilities
+
+
+import com.example.catchok_apps.firebase.Goal
+
+fun calculateDailyCalories(
+    currentWeight: Double?,
+    goalWeight: Double?,
+    age: Int?,
+    height: Double?,
+    goal: Goal?
+): Double {
+    val baseCalories: Double = 10.0
+    val weightFactor: Double = 6.25
+    val heightFactor: Double = 5.0
+    val ageFactor: Double = 5.0
+
+    val bmr = baseCalories +
+            (weightFactor * (currentWeight ?: 0.0)) +
+            (heightFactor * (height ?: 0.0)) -
+            (ageFactor * (age ?: 0))
+
+    val calorieAdjustment = when (goal) {
+        Goal.LOSE_WEIGHT -> -500.0
+        Goal.GAIN_MUSCLES -> 500.0
+        Goal.GET_FITTER -> 300.0
+        else -> 0.0
+    }
+
+    val dailyCalories = (bmr + calorieAdjustment)* goalWeight!! / currentWeight!!
+
+    return dailyCalories
+}
