fd3f077f1224707752afeb52e3c6cf162ea1bff4
Diyatt <120357743+Diyatt@users.noreply.github.com>
2023-12-18 19:17:33 +0600

    repowork

---
+++ app/src/main/java/com/example/catchok_apps/repository/workout/WorkoutViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.catchok_apps.repository.workout
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import dagger.hilt.android.lifecycle.HiltViewModel
+import com.example.catchok_apps.base.BaseViewModel
+import com.example.catchok_apps.repository.workout.model.Workout
+import javax.inject.Inject
+
+
+@HiltViewModel
+class WorkoutViewModel @Inject constructor(
+    private var repository: WorkoutRepository
+):BaseViewModel() {
+
+    private var _workoutListLiveData = MutableLiveData<List<Workout>?>()
+    var workoutListLiveData: LiveData<List<Workout>?> = _workoutListLiveData
+
+    fun getWorkouts(){
+        launch(
+            request = {
+                repository.getWorkoutList()
+            },
+            onSuccess = {
+                _workoutListLiveData.postValue(it)
+            }
+        )
+    }
+
+    fun getWorkoutsByTarget(target:String){
+        launch(
+            request = {
+                repository.getWorkoutListByTarget(target)
+            },
+            onSuccess = {
+                _workoutListLiveData.postValue(it)
+            }
+        )
+    }
+    fun setSortedWorkouts(sortedWorkouts: List<Workout>) {
+        _workoutListLiveData.value = sortedWorkouts
+    }
+
+}
+//    private fun getData() {
+//        val data = repository.getWorkoutList()
+//        _workoutListLiveData.postValue(data)
+//    }
