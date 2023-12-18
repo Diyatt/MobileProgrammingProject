ca65983d2892d864816b7fd3d92f46bb349b86de
alipbekovich <erbolruskulbekvipp@gmail.com>
2023-12-18 17:52:54 +0600

    1

---
+++ app/src/main/java/com/example/catchok_apps/fragments/EditProfileFragment.kt
@@ -0,0 +1,101 @@
+package com.example.catchok_apps.fragments
+
+import android.net.Uri
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import dagger.hilt.android.AndroidEntryPoint
+import com.example.catchok_apps.base.BaseFragment
+import com.example.catchok_apps.databinding.FragmentEditProfileBinding
+import com.example.catchok_apps.firebase.UserDao
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class EditProfileFragment : BaseFragment<FragmentEditProfileBinding>(FragmentEditProfileBinding::inflate) {
+
+    @Inject
+    lateinit var firebaseAuth: FirebaseAuth
+
+    @Inject
+    lateinit var storageReference: StorageReference
+    private var imageUri: Uri? = null
+
+    @Inject
+    lateinit var userDao:UserDao
+
+
+
+    override var showBottomNavigation: Boolean = false
+
+    override fun onBindView() {
+        super.onBindView()
+        val user = firebaseAuth.currentUser
+        val uid = user?.uid
+        storageReference = FirebaseStorage.getInstance().getReference("Users/" + "${uid}")
+        with(binding) {
+
+            avatar.setOnClickListener {
+                resultLauncher.launch("image/*")
+            }
+
+            backBtn.setOnClickListener {
+                findNavController().popBackStack()
+            }
+            saveBtn.setOnClickListener {
+                binding.loading.isVisible = true
+                if(imageUri != null){
+                    uploadProfilePic()
+                }
+                updateProfileInfo()
+            }
+        }
+    }
+
+    private val resultLauncher = registerForActivityResult(
+        ActivityResultContracts.GetContent()){
+        binding.avatar.setImageURI(it)
+        imageUri = it
+    }
+
+    private fun uploadProfilePic(){
+        imageUri?.let {
+            storageReference.putFile(it).addOnSuccessListener {task->
+                task.metadata?.reference?.downloadUrl?.addOnSuccessListener {uri->
+                    val imgUrl = uri.toString()
+                    userDao.saveProfilePic(imgUrl)
+                    binding.loading.isVisible = false
+                    showCustomDialog("Success", "Information saved")
+
+                }
+
+
+            }.addOnFailureListener{
+                Toast.makeText(requireContext(), "Unable to upload profile pic", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    private fun updateProfileInfo(){
+        if(binding.etBio.text?.isNotEmpty() == true){
+            binding.tilBio.isErrorEnabled = false
+            userDao.saveBio(binding.etBio.text.toString())
+        }
+        else{
+            binding.tilBio.error = "Fill up"
+            binding.tilBio.isErrorEnabled = true
+        }
+
+        if(binding.etGoalWeight.text?.isNotEmpty() == true){
+            binding.tilGoalweight.isErrorEnabled = false
+            userDao.saveGoalWeight(binding.etGoalWeight.text.toString().toInt())
+        }
+        else{
+            binding.tilGoalweight.error = "Fill up"
+            binding.tilGoalweight.isErrorEnabled = true
+        }
+        binding.loading.isVisible = false
+    }
+}
