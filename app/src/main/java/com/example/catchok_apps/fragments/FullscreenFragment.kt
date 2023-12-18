5ebc53d447d26ceafb4fdb2e98d548346430732a
Diyatt <120357743+Diyatt@users.noreply.github.com>
2023-12-18 16:47:49 +0600

    Fullscreen

---
+++ app/src/main/java/com/example/catchok_apps/fragments/FullscreenFragment.kt
@@ -0,0 +1,25 @@
+package com.example.catchok_apps.fragments
+
+
+import androidx.navigation.fragment.navArgs
+import androidx.transition.TransitionInflater
+import com.bumptech.glide.Glide
+import kz.just_code.devmuscles.base.BaseFragment
+import kz.just_code.devmuscles.databinding.FragmentFullscreenBinding
+
+class FullscreenFragment:BaseFragment<FragmentFullscreenBinding>(FragmentFullscreenBinding::inflate) {
+    private val args:FullscreenFragmentArgs by navArgs()
+    override var showBottomNavigation: Boolean = false
+    override fun onBindView() {
+        super.onBindView()
+        Glide.with(requireContext())
+            .load(args.workoutItem.gifUrl)
+            .into(binding.image)
+
+
+        binding.image.transitionName = "gif_${args.workoutItem.id}"
+
+        sharedElementEnterTransition = TransitionInflater.from(requireContext()).inflateTransition(android.R.transition.move)
+    }
+
+}
