1119febc8a29d25361a165637c5d674fe93b1323
alipbekovich <erbolruskulbekvipp@gmail.com>
2023-12-18 18:41:00 +0600

    network

---
+++ app/src/main/java/com/example/catchok_apps/network/gpt/GptApi.kt
@@ -0,0 +1,18 @@
+package com.example.catchok_apps.network.gpt
+
+
+import com.example.catchok_apps.repository.gpt.model.ChatResponse
+import com.example.catchok_apps.repository.gpt.model.RequestBody
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.POST
+
+
+
+interface GptApi {
+    @POST("chat/completions")
+    suspend fun getPrompt(
+        @Body requestBody: RequestBody,
+    ): Response<ChatResponse>
+}
---
+++ app/src/main/java/com/example/catchok_apps/network/gpt/NetworkGptModule.kt
@@ -0,0 +1,58 @@
+package com.example.catchok_apps.network.gpt
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import com.example.catchok_apps.utilities.RetryInterceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.internal.http.RetryAndFollowUpInterceptor
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkGptModule {
+    private const val baseUrl = "https://api.openai.com/v1/"
+    private val token = "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"
+
+
+    val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+    private var client: OkHttpClient = OkHttpClient
+        .Builder()
+        .addInterceptor(loggingInterceptor)
+        .addInterceptor(RetryInterceptor())
+        .readTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .addInterceptor { chain ->
+            val newRequest: Request =
+                chain.request()
+                    .newBuilder()
+                    .addHeader("Content-Type", "application/json")
+                    .addHeader("Authorization", "Bearer $token").build()
+            chain.proceed(newRequest)
+        }.build()
+
+
+    fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun getApi(): GptApi {
+        return getRetrofit()
+            .create(GptApi::class.java)
+    }
+}
