3e78df584fad6715503ce9118ed0c03e82c20cff
Diyatt <120357743+Diyatt@users.noreply.github.com>
2023-12-18 18:26:18 +0600

    commit

---
+++ app/src/main/java/com/example/catchok_apps/adapter/ItemRecommendedWorkoutAdapter.kt
@@ -0,0 +1,61 @@
+package com.example.catchok_apps.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.bumptech.glide.Glide
+import com.example.catchok_apps.base.BaseWorkoutViewHolder
+import com.example.catchok_apps.databinding.ItemRecommendedWorkoutBinding
+import com.example.catchok_apps.fragments.titlecaseFirstChar
+import com.example.catchok_apps.repository.workout.model.Workout
+
+class ItemRecommendedWorkoutAdapter:ListAdapter<Workout, BaseWorkoutViewHolder<*>>(WorkoutDiffUtils()) {
+
+
+    var itemClick:((Workout)-> Unit)? = null
+    class WorkoutDiffUtils:DiffUtil.ItemCallback<Workout>(){
+        override fun areItemsTheSame(oldItem: Workout, newItem: Workout): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Workout, newItem: Workout): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseWorkoutViewHolder<*> {
+        return WorkoutViewHolder(
+            ItemRecommendedWorkoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: BaseWorkoutViewHolder<*>, position: Int) {
+        holder.bindView(getItem(position))
+    }
+
+
+    inner class WorkoutViewHolder(binding:ItemRecommendedWorkoutBinding):BaseWorkoutViewHolder<ItemRecommendedWorkoutBinding>(binding){
+        override fun bindView(item: Workout) {
+            with(binding){
+                val workoutTitle = item.name?.titlecaseFirstChar()
+                if(workoutTitle?.length!! <15){
+                    title.text = workoutTitle
+                }
+                else{
+                    title.text = workoutTitle.substring(0,15)
+                }
+                target.text = "${item.target?.titlecaseFirstChar()} Workout"
+                equipment.text = item.equipment?.titlecaseFirstChar()
+                Glide.with(itemView.context)
+                    .load(item.gifUrl)
+                    .into(gif)
+            }
+            itemView.setOnClickListener {
+                itemClick?.invoke(item)
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/catchok_apps/adapter/ItemWorkoutAdapter.kt
@@ -0,0 +1,138 @@
+package com.example.catchok_apps.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.example.catchok_apps.R
+import com.example.catchok_apps.base.BaseWorkoutViewHolder
+import com.example.catchok_apps.databinding.ItemWorkoutBinding
+import com.example.catchok_apps.fragments.titlecaseFirstChar
+import com.example.catchok_apps.repository.workout.model.Workout
+
+class ItemWorkoutAdapter:ListAdapter<Workout, BaseWorkoutViewHolder<*>>(WorkoutDiffUtils()) {
+
+
+    var itemClick:((Workout, Map<View, String>)-> Unit)? = null
+    class WorkoutDiffUtils:DiffUtil.ItemCallback<Workout>(){
+        override fun areItemsTheSame(oldItem: Workout, newItem: Workout): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Workout, newItem: Workout): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseWorkoutViewHolder<*> {
+        return WorkoutViewHolder(
+            ItemWorkoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: BaseWorkoutViewHolder<*>, position: Int) {
+        holder.bindView(getItem(position))
+    }
+
+
+    inner class WorkoutViewHolder(binding:ItemWorkoutBinding):BaseWorkoutViewHolder<ItemWorkoutBinding>(binding){
+
+
+        override fun bindView(item: Workout) {
+            with(binding){
+                val workoutTitle = item.name?.titlecaseFirstChar()
+                if(workoutTitle?.length!! <15){
+                    title.text = workoutTitle
+                }
+                else{
+                    title.text = workoutTitle.substring(0,15)
+                }
+                type.text = "${item.target?.titlecaseFirstChar()} Workout"
+                equipment.text = item.equipment?.titlecaseFirstChar()
+                type.transitionName = "type_${item.id}"
+                title.transitionName = "title_${item.id}"
+                var color:Int? = null
+                var chipColor:Int? = null
+                var imageRes:Int? = null
+                var padding:Int = 0
+                completed.isVisible = false
+                when (item.target) {
+                    "abs" -> {
+                        imageRes = R.drawable.ob_2
+                        color = R.color.card_view_1
+                        chipColor = R.color.chip_color1
+                        padding = 200
+                    }
+                    "delts" -> {
+                        imageRes = R.drawable.nobg_girl
+                        color = R.color.card_view_2
+                        chipColor = R.color.chip_color2
+
+
+                    }
+                    "biceps" -> {
+                        imageRes = R.drawable.biceps_nobg
+                        color = R.color.card_view_3
+                        chipColor = R.color.chip_color3
+
+
+                    }
+                    "calves" -> {
+                        imageRes = R.drawable.calves_nobg
+                        color = R.color.card_view_2
+                        chipColor = R.color.chip_color2
+
+
+                    }
+                    "quads" -> {
+                        imageRes = R.drawable.legs_nobg
+                        color = R.color.card_view_2
+                        chipColor = R.color.chip_color2
+
+                    }
+                    "triceps" -> {
+                        imageRes = R.drawable.triceps_nobg
+                        color = R.color.card_view_3
+                        chipColor = R.color.chip_color3
+
+                    }
+                    "lats" -> {
+                        imageRes = R.drawable.lats_nobg
+                        color = R.color.card_view_3
+                        chipColor = R.color.chip_color3
+                    }
+                    "glutes"->{
+                        imageRes = R.drawable.girl1_nobg
+                        color = R.color.card_view_3
+                        chipColor = R.color.chip_color3
+                    }
+                    else -> {
+                        imageRes= R.drawable.the_rest_nobg
+                        color = R.color.card_view_1
+                        chipColor = R.color.chip_color1
+
+                    }
+                }
+                image.setImageResource(imageRes)
+                image.transitionName = "image_${item.id}"
+                image.setPadding(0, padding, 0, 0)
+                root.setCardBackgroundColor(ContextCompat.getColor(root.context, color ))
+                type.setChipBackgroundColorResource(chipColor)
+
+            }
+            itemView.setOnClickListener {
+                itemClick?.invoke(item, mapOf(
+                    binding.title to "title_${item.id}",
+                    binding.type to "type_${item.id}",
+                    binding.image to "image_${item.id}"
+                ))
+            }
+        }
+    }
+}
+
+
